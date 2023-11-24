6b6cb6286c1b35629ce6cf240e0c255a9b5e7dc1
Shanto Noor <shantonoor900@gmail.com>
2023-11-25 01:09:32 +0600

    nav 50% complete

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import { Container } from "@mui/material";
+import Footer from "../components/Footer";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Outlet />
+      </Container>
+      {/* <Footer /> */}
+    </>
+  );
+};
+
+export default MainLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./main.css";
 
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout.jsx";
+import Home from "./pages/Home.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
